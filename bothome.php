6081dfe90497b9de9e26089372a44ebf3c32749f
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2019-10-29 18:17:08 +0700

    Create bothome.php

---
+++ bothome.php
@@ -0,0 +1,21 @@
+<?php
+include 'class_ig.php';
+
+			$data_login = array(
+		'username' => 'username lu',
+		'csrftoken'	=> 'csrftoken lu',
+		'sessionid'	=> 'sessionid lu'
+	);
+			    $profile = getHome($data_login);
+				$data_array = json_decode($profile);
+			    $result = $data_array->user->edge_web_feed_timeline;
+			    foreach ($result->edges as $items) {
+				$id = $items->node->id;
+				$username = $items->node->owner->username;
+				$like = like($id, $data_login);
+				if ($like['status'] == 'error') {
+				echo '[+] Username: '.$username.' | Error Like' . PHP_EOL;
+				}else{
+				echo '[+] Username: '.$username.' |  Like Success'. PHP_EOL;;
+				}
+				}		
