a2cd49ea180f3fa9eb17a5d0ff57a2f1409018eb
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2019-11-11 01:58:20 +0700

    Create botlikehome.php

---
+++ botlikehome.php
@@ -0,0 +1,33 @@
+<?php
+//ikiganteng
+include 'class_ig.php';
+error_reporting(0);
+clear();
+copycat();
+$u = getUsername();
+$p = getPassword();
+$sleep = getComment('Enter your sleep here (Min 600): ');
+// $u = 'indiramayasari7'; $p = 'Anonymous1704!'; $text = 'Hi!';
+echo '############################################################' . PHP_EOL . PHP_EOL;
+$login = login($u, $p);
+if ($login['status'] == 'success') {
+echo '[ * ] Login as '.$login['username'].' Success!' . PHP_EOL;
+$data_login = array('username' => $login['username'],'csrftoken'	=> $login['csrftoken'],'sessionid'	=> $login['sessionid']);
+				while(true){
+		    $profile = getHome($data_login);
+				$data_array = json_decode($profile);
+			  $result = $data_array->user->edge_web_feed_timeline;
+			  foreach ($result->edges as $items) {
+				$id = $items->node->id;
+				$username = $items->node->owner->username;
+				$like = like($id, $data_login);
+				if ($like['status'] == 'error') {
+				echo '[+] Username: '.$username.' | Error Like' . PHP_EOL;
+				}else{
+				echo '[+] Username: '.$username.' |  Like Success'. PHP_EOL;;
+				}
+				}
+				echo '[+] Tidur selama '.$sleep.' detik [+]'. PHP_EOL;;
+				sleep($sleep);
+                }
+				} else echo json_encode($login);
