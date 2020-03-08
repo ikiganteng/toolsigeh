e6266f3fd61ac7eefb69978b49fdecbcf1bf6116
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2020-03-09 03:54:28 +0700

    Update unfollall.php

--- unfollall.php
+++ unfollall.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
 include 'class_ig.php';
 error_reporting(0);
 
@@ -7,7 +8,7 @@ copycat();
 $u = getUsername();
 $p = getPassword();
 echo PHP_EOL;
-$sleep = getComment('Sleep in seconds: ');
+//$sleep = getComment('Sleep in seconds: ');
 echo '############################################################' . PHP_EOL . PHP_EOL;
 $login = login($u, $p);
 if ($login['status'] == 'success') {
@@ -18,35 +19,25 @@ if ($login['status'] == 'success') {
 		'csrftoken'	=> $login['csrftoken'],
 		'sessionid'	=> $login['sessionid']
 	);
-	
 	$data_target = findProfile($u, $data_login);
 	echo color()["LG"].'[ # ] Name: '.$data_target['fullname'].' | Followers: '.$data_target['followers'] .' | Following: '.$data_target['following'] . PHP_EOL;
 	if ($data_target['status'] == 'success') {
-
-		$cmt = 0;
-		for ($i=1; $i < $data_target['following']; $i++) { 
-
-			$profile = getFollowing($u , $data_login, $i, 1);
+			$profile = getFollowing($u , $data_login, $data_target['following'], 1);
 			foreach ($profile as $rs) {
-
 				$username = $rs->username;
-				$unfollow = unfollow($username, $data_login);
+				$id_user = $rs->id;
+				$unfollow = unfollows($id_user, $data_login);
 				if ($unfollow['status'] == 'success') {
-					
-					echo color()["LG"].'[ > ] Unfollow "'.$username.'" Success!';
-					sleep($sleep);
+					echo color()["CY"].'[ > ] '.date('H:i:s').' Unfollow @'.$username.' Success!';
+					sleep(rand(70,100));
 					echo PHP_EOL;
 				}else{
-
-					echo color()["LR"].'[ ! ] Unfollow "'.$username.'" Failed!';
-					sleep($sleep);
+					echo color()["LR"].'[ ! ] '.date('H:i:s').' Unfollow @'.$username.' Failed! Throttled! Resting during 10 minutes before try again.';
+					sleep(10*60);		
 					echo PHP_EOL;
 				}
 
 			}
-
-		}
-
 	}else{
 
 		echo 'Error!';
@@ -54,5 +45,5 @@ if ($login['status'] == 'success') {
 	}
 }else{
 
-	echo color()["LR"].'[ * ] Login as '.$login['username'].' Failed!' . PHP_EOL;
+		echo color()['LR'].'Error: '.ucfirst($login['details'])."" . PHP_EOL;
 }
